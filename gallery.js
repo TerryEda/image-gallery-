01b4da38a23ec38457f8cb471ae2623513947aed
terryEda <okwarieda@gmail.com>
2022-11-30 01:02:33 +0100

    initial commit

---
+++ gallery.js
@@ -0,0 +1,50 @@
+const change = document.getElementById("image1");
+change.addEventListener("click", (Event) => {
+    Event.target.style.background = "red";
+    Event.target.style.color = "white";
+
+});
+const change2 = document.getElementById("image2");
+change2.addEventListener("click", (Event) => {
+    Event.target.style.background = "blue";
+    Event.target.style.color = "white";
+
+});
+const change3 = document.getElementById("image3");
+change3.addEventListener("click", (Event) => {
+    Event.target.style.background = "red";
+    Event.target.style.color = "white";
+
+});
+const change4 = document.getElementById("image4");
+change4.addEventListener("click", (Event) => {
+    Event.target.style.background = "blue";
+    Event.target.style.color = "white";
+
+});
+const change5 = document.getElementById("image5");
+change5.addEventListener("click", (Event) => {
+    Event.target.style.background = "red";
+    Event.target.style.color = "white";
+
+});
+
+const change6 = document.getElementById("image6");
+change6.addEventListener("click", (Event) => {
+    Event.target.style.background = "blue";
+    Event.target.style.color = "white";
+
+});
+const change7 = document.getElementById("image7");
+change7.addEventListener("click", (Event) => {
+    Event.target.style.background = "red";
+    Event.target.style.color = "white";
+
+});
+const change8 = document.getElementById("image8");
+change8.addEventListener("click", (Event) => {
+    Event.target.style.background = "blue";
+    Event.target.style.color = "white";
+
+});
+
